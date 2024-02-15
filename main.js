d9e26a1a3cfea0ce9c82c147305dc20f6f553323
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2024-02-14 23:38:02 -0500

    update

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 import { setupCounter } from "./counter.js";
-import javascriptLogo from "./javascript.svg";
-import "./style.css";
-import viteLogo from "/vite.svg";
 
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="./vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      <img src="./javascript.svg" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Hackers!</h1>
     <div class="card">
